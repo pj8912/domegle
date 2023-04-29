4bc3c3cde053860cae21703920233114ff886cb0
pj8912 <jpintofun2b@gmail.com>
2023-04-29 12:37:41 +0530

    Home UI update

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template
 import os
 import sys
 
-
-
-
 app = Flask(__name__, static_url_path='/static') #  we set the static_url_path parameter to '/static', which tells Flask to serve static files from a directory named static in the root directory of our application.
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
 
 
 if __name__ == "__main__":
